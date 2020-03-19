418377ec4468ce64d58e6335b9edfee6ac517288
Farkhan Azmi <fliazmi@gmail.com>
2020-03-20 01:37:02 +0700

    fix

--- test.php
+++ test.php
@@ -3,10 +3,10 @@
 // $countryDataFile = fopen("countryData.json", "r") or die("Unable to open file!");
 // $countryData = json_decode(fgets($countryDataFile), true);
 // fclose($countryDataFile);
-$negara = isset($a[1]) ? $a[1] : 'china';
+$negara = isset($a[1]) ? $a[1] : 'indonesia';
 $data = file_get_contents("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=(Country_Region%3D'$negara')&returnGeometry=false&spatialRef=esriSpatialRelIntersects&outFields=*&orderByFields=Country_Region%20asc,Province_State%20asc&resultOffset=0&resultRecordCount=250&cacheHint=false");
 $data= json_decode($data);
-var_dump($data);
+// var_dump($data);
 
 $total =0;
 $positif =0;
@@ -19,7 +19,7 @@ foreach ( $data->features as $value) {
     $sembuh +=$rawResponse->Recovered;
     $mati +=$rawResponse->Deaths;
 }
-echo $data->features[0]->attributes->Last_Update;
+echo date("Y-m-d H:i:s", substr($data->features[0]->attributes->Last_Update, 0, 10));
 echo " ";
 echo $total ." ".$positif." ".$sembuh." ".$mati;
 // $response="";
