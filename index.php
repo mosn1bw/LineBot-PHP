414549674f133c7fcba607a098fb3f48f511fa94
Farkhan Azmi <fliazmi@gmail.com>
2020-03-20 00:23:30 +0700

    covid19 update bot

--- index.php
+++ index.php
@@ -77,6 +77,18 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
             $groupId     = $event['source']['groupId'];
             $result = $bot->replyText($event['replyToken'], $groupId);
             break;
+            
+          case '/covidupdatecoredata':
+            $data =  file_get_contents('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=(Confirmed%3E%200)%20OR%20(Deaths%3E0)%20OR%20(Recovered%3E0)&returnGeometry=false&spatialRef=esriSpatialRelIntersects&outFields=*&orderByFields=Country_Region%20asc,Province_State%20asc&resultOffset=0&resultRecordCount=250&cacheHint=false');
+            $data = json_decode($data);
+            $countryData=[];
+            foreach ($data->features as $value) {
+                $countryData[strtolower($value->attributes->Country_Region)]['Lat']=(int)$value->attributes->Lat;
+                $countryData[strtolower($value->attributes->Country_Region)]['Long_']=(int)$value->attributes->Long_;
+            }
+            $myfile = fopen("countryData.json", "w") or die("Unable to open file!");
+            fwrite($myfile, json_encode($countryData));
+            break;
 
           case '/covid':
             $countryDataFile = fopen("countryData.json", "r") or die("Unable to open file!");
